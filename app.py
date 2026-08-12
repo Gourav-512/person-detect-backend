ee5740b858e8b0660850650c9a8741534e56fcd4
Gourav Salunkhe <137747550+Gourav-512@users.noreply.github.com>
2026-08-12 23:01:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,7 @@ def detect():
         "image_base64": img_base64
     })
     
+    
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
